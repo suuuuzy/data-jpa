842acde22a93fb0d2d5f61a929494ac7ecc688c8
suuuuzy <jangsooji91@gmail.com>
2022-11-05 17:01:44 +0900

    Projection

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -73,4 +73,7 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String username);
+
+    <T> List<T> findProjectionsByUsername(String username, Class<T> type);
+
 }
